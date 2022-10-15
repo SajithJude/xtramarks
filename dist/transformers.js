3db1f6434058bdfc51b05b76c09f8bc8ce479226
SajithJude <judesajith.aj@gmail.com>
2022-10-15 17:48:39 +0530

    kink

--- dist/transformers.js
+++ dist/transformers.js
@@ -24,9 +24,9 @@ class AutoModelForSeq2SeqLM extends PretrainedModel {
         const modelIdParts = modelId.split('/');
         const modelName = modelIdParts[modelIdParts.length - 1];
         const suffix = "-quantized";
-        const encoderUrl = "https://sajithjude.github.io/xtramarks/dist/t5_squad_v1-encoder-quantized.onnx";
-        const initDecoderUrl = "https://sajithjude.github.io/xtramarks/dist/t5_squad_v1-init-decoder-quantized.onnx";
-        const decoderUrl = "https://sajithjude.github.io/xtramarks/dist/t5_squad_v1-decoder-quantized.onnx";
+        const encoderUrl = `${modelsPath}/${modelName}-encoder${suffix}.onnx`;
+        const initDecoderUrl = `${modelsPath}/${modelName}-init-decoder${suffix}.onnx`;
+        const decoderUrl = `${modelsPath}/${modelName}-decoder${suffix}.onnx`;
 
         const progressMax = 4;
         let progress = 0;
